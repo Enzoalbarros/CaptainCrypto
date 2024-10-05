117463ef9a6e11aa2fbfbcfa812531e51813ce67
PAULO <cs.paulo.fn@gmail.com>
2024-10-05 16:19:57 -0400

    homepage aesthetic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,41 +17,49 @@ import Estimator from './pages/Estimator';
 
 function App() {
   return (
-    <div>
+    <div className='bg-blue-300'>
         <div>
           {/* This is how you make a route */}
           <Router>
             <Routes>
               {/* Route for the home page */}
               <Route path="/" element={
-                <div className="flex items-center justify-center mt-8">
-                  <div className='text-center '>
-                  <h1 className="text-8xl font-bold mb-20">The Crypto Captain</h1>
-                  <img 
-                    src="/images/pirateLogo.webp"
-                    alt="logo" 
-                    className="mx-auto mb-8 w-48 h-48 object-cover rounded-full"
-                  />
-                  <ul className='text-4xl'>
+                <div className="flex items-center justify-center">
+                  <div className='text-center mt-8'>
+                    <img 
+                      src="/images/pirateLogo.webp"
+                      alt="logo" 
+                      className="mx-auto mb-8 w-48 h-48 object-cover rounded-full"
+                    />
+                  <h1 className="text-8xl font-bold mb-20 font-mono text-red-900">The Crypto Captain</h1>
+                  <ul className='text-4xl font-mono font-bold'>
                     <li>
-                    <Link to="/information">
-                    <button className="btn m-12 bg-gray-500">Go to Information</button>
-                  </Link>
+                      <div className='m-12 bg-gray-500 rounded-3xl'>
+                        <Link to="/information">
+                          <button className="btn m-8 text-black">Information</button>
+                        </Link>
+                      </div>
                     </li>
                     <li>
-                    <Link to="/calculator">
-                    <button className="btn m-12 bg-gray-500">Go to Calculator</button>
-                  </Link>
+                      <div className='m-12 bg-gray-500 rounded-3xl'>  
+                          <Link to="/calculator">
+                            <button className="btn m-8 text-black">Calculator</button>
+                          </Link>
+                      </div>
                     </li>
                     <li>
-                    <Link to="/opportunities">
-                    <button className="btn m-12 bg-gray-500">Go to Opportunities</button>
-                  </Link>
+                    <div className='m-12 bg-gray-500 rounded-3xl'>
+                        <Link to="/opportunities">
+                          <button className="btn m-8 text-black">Opportunities</button>
+                        </Link>
+                    </div>
                     </li>
                     <li>
-                    <Link to="/estimator">
-                    <button className="btn m-12 bg-gray-500">Go to Estimator</button>
-                  </Link>
+                      <div className='m-12 bg-gray-500 rounded-3xl'>
+                        <Link to="/estimator">
+                          <button className="btn m-8 text-black">Estimator</button>
+                        </Link>
+                      </div>
                     </li>
                   </ul>
                   </div>
